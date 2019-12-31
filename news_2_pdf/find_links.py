647e80957705d89212f5b9bde5ce7dc694d58b23
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-31 12:56:34 -0800

    support nyt

--- news_2_pdf/find_links.py
+++ news_2_pdf/find_links.py
@@ -4,13 +4,31 @@ import yaml
 from telegram_util import matchKey
 import cached_url
 
-def findLinks(soup, news_source='bbc'):
+def getItems(soup, news_source):
+	if news_source == 'bbc':
+		return soup.find_all('a', class_='title-link')
+	for x in soup.find_all():
+		if not x.attrs:
+			continue
+		if 'Headline' not in str(x.attrs.get('class')):
+			continue
+		for y in x.find_all('a'):
+			yield y
+
+def getDomain(news_source):
+	if news_source == 'bbc':
+		return 'https://www.bbc.co.uk'
+	return 'https://cn.nytimes.com'
+
+def findLinks(news_source='bbc'):
 	source_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'source.yaml')
 	with open(source_filename) as f:
 		source = yaml.load(f, Loader=yaml.FullLoader)
 	soup = BeautifulSoup(cached_url.get(source[news_source]), 'html.parser')
 	links = {}
-	for item in soup.find_all('a', class_='title-link'):
+	domain = getDomain(news_source)
+
+	for item in getItems(soup, news_source):
 		if not item.text or not item.text.strip():
 			continue
 		name = item.text.strip()
@@ -22,5 +40,5 @@ def findLinks(soup, news_source='bbc'):
 			continue
 		links[name] = item['href'].strip()
 		if not '://' in links[name]:
-			links[name] = 'https://www.bbc.co.uk' +  links[name]
+			links[name] =  domain +  links[name]
 	return links
