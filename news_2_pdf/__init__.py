59cb2fe67efbdf2b0defb265d5fba2c8dd481848
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-31 14:43:57 -0800

    commit

--- news_2_pdf/__init__.py
+++ news_2_pdf/__init__.py
@@ -4,18 +4,12 @@
 name = 'news_2_pdf'
 
 import os
-import argparse
 from .find_links import findLinks
 from .article import getArticleHtml
 from .index import getIndexHtml
 from datetime import date
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-ebook_convert_app')
-args = parser.parse_args()
-if args.ebook_convert_app:
-	ebook_convert_app = args.ebook_convert_app
-elif os.name == 'posix':
+if os.name == 'posix':
 	ebook_convert_app = '/Applications/calibre.app/Contents/MacOS/ebook-convert'
 else:
 	ebook_convert_app = 'ebook-convert'
